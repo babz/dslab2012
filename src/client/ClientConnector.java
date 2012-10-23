1c5313d15b895f78029daa3a848bf79b53431baa
Babz <Babz@Babz-Laptop.(none)>
2012-10-23 13:59:52 +0200

    client server are communicating

--- src/client/ClientConnector.java
+++ src/client/ClientConnector.java
@@ -8,7 +8,7 @@ import org.apache.log4j.Logger;
 
 public class ClientConnector {
 	
-	static Logger log = Logger.getLogger(ClientConnector.class);
+	private static final Logger log = Logger.getLogger(ClientConnector.class);
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -19,24 +19,26 @@ public class ClientConnector {
 		BufferedReader in = null;
 
 		try {
-			echoSocket = new Socket("stockholm.vitalab.tuwien.ac.at", 9000);
+			echoSocket = new Socket("localhost", 13460);
 			out = new PrintWriter(echoSocket.getOutputStream(), true);
+//			Stream for listening to the servers' answer
 			in = new BufferedReader(new InputStreamReader(
 					echoSocket.getInputStream()));
 			log.info("socket, pw and br created");
 		} catch (UnknownHostException e) {
-			System.err.println("Don't know about host stockholm");
+			System.err.println("Don't know about host");
 		} catch (IOException e) {
 			System.err.println("Couldn't get I/O for "
-					+ "the connection to: stockholm.");
+					+ "the connection to host.");
 		}
 
+//		Stream to read user input
 		BufferedReader stdIn = new BufferedReader(
 				new InputStreamReader(System.in));
 
 		String userInput;
 		while ((userInput = stdIn.readLine()) != null) {
-			log.info("pass request to server");
+			log.info("pass user request to server");
 			out.println(userInput);
 			log.info("catch server answer:");
 			System.out.println(in.readLine());
--- src/server/AuctionManager.java
+++ src/server/AuctionManager.java
@@ -1,5 +1,17 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import client.ClientConnector;
+
 /**
  * Manage auctions created by clients
  * @author Babz
@@ -7,4 +19,51 @@ package server;
  */
 public class AuctionManager {
 
+	private static final Logger log = Logger.getLogger(AuctionManager.class);
+
+	public static void main(String[] args) throws IOException {
+
+		BasicConfigurator.configure();
+
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(13460);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port");
+		}
+
+		Socket clientSocket = null;
+		try {
+			clientSocket = serverSocket.accept(); 
+			log.info("client socket accepted");
+		} catch (IOException e) {
+			System.err.println("Accept failed.");
+		}
+
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(
+						clientSocket.getInputStream()));
+		log.info("socket, pw and br created");
+		
+		String inputLine, outputLine;
+//		KnockKnockProtocol kkp = new KnockKnockProtocol();
+//
+//		outputLine = kkp.processInput(null);
+//		out.println(outputLine);
+
+		int tmp = 0;
+		while ((inputLine = in.readLine()) != null) {
+//			outputLine = kkp.processInput(inputLine);
+//			out.println(outputLine);
+//			if (outputLine.equals("Bye."))
+//				break;
+			log.info("return answer");
+			out.println("Hello babz" + ++tmp);
+		}
+		out.close();
+		in.close();
+		clientSocket.close();
+		serverSocket.close();
+	}
 }
