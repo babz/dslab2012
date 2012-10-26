53f1b163eee82a1a865db22bc6c798dd9bc8d55f
Babz <Babz@Babz-Laptop.(none)>
2012-10-26 23:28:18 +0200

    NOW multiple clients work

---
+++ src/server/ClientListener.java
@@ -0,0 +1,59 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.log4j.Logger;
+
+public class ClientListener implements Runnable {
+
+	private static final Logger log = Logger.getLogger(ClientListener.class);
+
+	private PrintWriter out;
+	private BufferedReader in;
+
+	private Socket clientSocket;
+
+	public ClientListener(Socket client) throws IOException {
+		clientSocket = client;
+	}
+
+	@Override
+	public void run() {
+		try {
+			out = new PrintWriter(clientSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+		} catch (IOException e1) {
+			log.error("problems with streams");
+		}
+		log.info("socket, pw and br created");
+
+		String inputLine;
+		int tmp = 0;
+		try {
+			while ((inputLine = in.readLine()) != null) {
+				log.info("return answer");
+				out.println("Hello babz" + ++tmp);
+			}
+		} catch (IOException e) {
+			log.error("problems with writing an answer to client");
+		}
+		closeClientSocket(clientSocket);
+	}
+
+	private void closeClientSocket(Socket client){
+		out.close();
+		try {
+			in.close();
+			client.close();
+		} catch (IOException e) {
+			log.error("problems closing client socket and streams");
+		}
+	}
+}
+
