425e2b2706655eef9513e279d770e4518c20129b
Babz <Babz@Babz-Laptop.(none)>
2012-10-27 01:38:45 +0200

    handling of wrong args in the main classes

--- src/client/ClientConnector.java
+++ src/client/ClientConnector.java
@@ -7,13 +7,40 @@ import org.apache.log4j.Logger;
 
 
 public class ClientConnector {
-	
-	private static final Logger log = Logger.getLogger(ClientConnector.class);
-	
-	public static void main(String[] args) throws IOException {
-		
+
+	private static final Logger LOG = Logger.getLogger(ClientConnector.class);
+	private static final int NO_OF_ARGS = 3;
+
+	/**
+	 * Starts the client
+	 * @param args String array contains server host/ip address, tcp port and udp port
+	 */
+	public static void main(String[] args) {
+
+		//logger
 		BasicConfigurator.configure();
-		
+
+		ClientConnector client = new ClientConnector();
+
+		if(args.length != NO_OF_ARGS) {
+			System.out.println("supposed no of args:" + NO_OF_ARGS);
+			return;
+		}
+		//TODO check args types
+
+		String serverHost = args[0];
+		int tcpPort = Integer.parseInt(args[1]);
+		int udpPort = Integer.parseInt(args[2]);
+		client.startClient(serverHost, tcpPort, udpPort);
+	}
+	/**
+	 * Creates a socket on client side and opens print writer and buffered reader on it;
+	 * enables client to communicate with server
+	 * @param serverHost servers host or ip address
+	 * @param tcpPort tcp port of client and server respectively
+	 * @param udpPort udp port the client is listening on
+	 */
+	public void startClient(String serverHost, int tcpPort, int udpPort) {
 		Socket echoSocket = null;
 		PrintWriter out = null;
 		BufferedReader in = null;
@@ -21,10 +48,10 @@ public class ClientConnector {
 		try {
 			echoSocket = new Socket("localhost", 13460);
 			out = new PrintWriter(echoSocket.getOutputStream(), true);
-//			Stream for listening to the servers' answer
+			//			Stream for listening to the servers' answer
 			in = new BufferedReader(new InputStreamReader(
 					echoSocket.getInputStream()));
-			log.info("socket, pw and br created");
+			LOG.info("socket, pw and br created");
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host");
 		} catch (IOException e) {
@@ -32,22 +59,32 @@ public class ClientConnector {
 					+ "the connection to host.");
 		}
 
-//		Stream to read user input
+		//		Stream to read user input
 		BufferedReader stdIn = new BufferedReader(
 				new InputStreamReader(System.in));
 
 		String userInput;
-		while ((userInput = stdIn.readLine()) != null) {
-			log.info("pass user request to server");
-			out.println(userInput);
-			log.info("catch server answer:");
-			System.out.println(in.readLine());
+		try {
+			while ((userInput = stdIn.readLine()) != null) {
+				LOG.info("pass user request to server");
+				out.println(userInput);
+				LOG.info("catch server answer:");
+				System.out.println(in.readLine());
+			}
+		} catch (IOException e) {
+			LOG.error("couldnt receive server response");
 		}
 
+
 		out.close();
-		in.close();
-		stdIn.close();
-		echoSocket.close();
+		try {
+			in.close();
+			stdIn.close();
+			echoSocket.close();
+		} catch (IOException e) {
+			LOG.error("couldnt close socket and streams properly");
+		}
+
 	}
 }
 
--- src/server/AuctionManager.java
+++ src/server/AuctionManager.java
@@ -15,40 +15,63 @@ import org.apache.log4j.Logger;
  */
 public class AuctionManager {
 
-	private static final Logger log = Logger.getLogger(AuctionManager.class);
-	
+	private static final Logger LOG = Logger.getLogger(AuctionManager.class);
+
+	private static final int NO_OF_ARGS = 1;
+
 	private final ExecutorService threadpool = Executors.newCachedThreadPool(); 
-	
-	public static void main(String[] args) throws IOException {
+
+	/**
+	 * Starts the server with tcp port
+	 * @param args tcp port
+	 */
+	public static void main(String[] args) {
+
 		//logger
 		BasicConfigurator.configure();
-		
-		AuctionManager server1 = new AuctionManager();
-		server1.startServer();
+
+		AuctionManager server = new AuctionManager();
+
+		if(args.length != NO_OF_ARGS) {
+			System.out.println("supposed no of args:" + NO_OF_ARGS);
+			return;
+		}
+		//TODO check args type
+
+		int tcpPort = Integer.parseInt(args[0]);
+
+		server.startServer(tcpPort);
 	}
 
-	public void startServer() {		
-		
-		
+	/**
+	 * creates server sockets;
+	 * opens a thread for each incoming client connection request using a threadpool;
+	 * handles termination of threads and closes server socket
+	 * @param tcpPort
+	 */
+	public void startServer(int tcpPort) {		
+
+
 		ServerSocket serverSocket = null;
 		try {
-			serverSocket = new ServerSocket(13460);
+			serverSocket = new ServerSocket(tcpPort);
 			while (true) {				
 				threadpool.execute(new ClientListener(serverSocket.accept()));
-				log.info("client socket accepted");
+				LOG.info("client socket accepted");
 			}
 		} catch (IOException e) {
 			System.err.println("Could not listen on port / accept failed");
 		}
-		
+
 		try {
 			serverSocket.close();
-			log.info("Server socket closed.\n");
+			threadpool.shutdown();
+			LOG.info("Server socket closed.");
 		} catch (IOException e) {
-			log.error("could not close server socket");
+			LOG.error("could not close server socket");
 		}
 	}
-	
+
 }
 
 
